c27534ace55dba499f18946afb856f1985224baa
jimin <studyingJM2123@gmail.com>
2021-08-07 20:32:20 +0900

    variables

---
+++ src/tutorial/Variables.kt
@@ -0,0 +1,16 @@
+package tutorial
+
+fun main() {
+  val a: Int = 1
+  val b = 2
+  val c: Int
+  c = 3
+
+  var x = 5
+  x+=1
+  println(x)
+}
+
+private fun sum(a:Int, b:Int): Int {
+  return a + b
+}
